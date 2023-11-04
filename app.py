abf63a847cb1ba034e8c11149c54fd03dca47db8
3fdh-h <3175794509@qq.com>
2023-11-05 01:06:26 +0800

    完成了首页布局和文章阅读页面布局，将两个页面中的CSS样式分离出来了，导入了open-iconic图标库但是没有添加到git中

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def hello_world():  # put application's code here
-	return render_template('index.html')
+	return render_template('ArticleRead.html')
 
 
 if __name__ == '__main__':
